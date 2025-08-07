9d12d9fa96caa125a85da8940d96cfec3c9e5a40
Magnus <mazejumper01@gmail.com>
2025-08-07 10:46:29 +0200

    The hero for the home page is completed

---
+++ src/components/Home/HomeInfo.jsx
@@ -0,0 +1,65 @@
+export default function HomeInfo() {
+
+    return (
+        <>
+            <section className="InfoSection
+            
+
+            ">
+
+
+
+                <img src="" alt="" />
+
+                <article>
+                    <h2>Vi har fulgt danskerne hjem i snart 4 årtier</h2>
+
+                    <h3>Det synes vi siger noget om os!</h3>
+
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                         The point of using Lorem Ipsum is that it has normal distribution.</p>
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+
+                    <div>
+                        <img src="" alt="" />
+
+                        <p>4829</p>
+                        <p>boliger solgt</p>
+                    </div>
+
+                    <div>
+                        <img src="" alt="" />
+
+                        <p>158</p>
+                        <p>boliger til salg</p>
+                    </div>
+                </article>
+
+
+                <div>
+                    
+                    <div>
+                        <img src="" alt="" />
+
+                        <p>Bestil et salgstjek</p>
+                        <p>Med et Din Mægler Salgstjek bliver du opdateret på værdien af din bolig.</p>
+                    </div>
+
+                    <div>
+                        <img src="" alt="" />
+
+                        <p>74 butikker</p>
+                        <p>Hos Din Mægler er din bolig til salg i alle vores 74 butikker, som er fordelt rundt om i Danmark.</p>
+                    </div>
+
+                    <div>
+                        <img src="" alt="" />
+
+                        <p>Tilmeld køberkartotek</p>
+                        <p>Når du er tilmeldt vores køberkartotek, bliver du kontaktet inden en ny bolig bliver annonceret.</p>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
+import HomeHero from '../components/Home/HomeHero';
+
 export default function home() {
 
     return (
         <>
-        <h1>Hej</h1>
-        <h2>Velkommen til din mægler</h2>
-        <p>Her kan du finde din mægler, og se hvilke boliger de har til salg.</p>
-        <p>Vi håber du finder det du leder efter.</p>
+        <HomeHero />
         </>
     )
 }
